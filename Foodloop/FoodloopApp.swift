ed24daf2edee335a391995a988c21e835a9a2e20
PatrykJanNeubauer <patrykneubauerdev@gmail.com>
2025-06-02 20:40:40 +0200

    Initial Commit

---
+++ Foodloop/FoodloopApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodloopApp.swift
+//  Foodloop
+//
+//  Created by Patryk Neubauer on 02/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct FoodloopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
